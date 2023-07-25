5dbec27cd18cb6277296dfbe7f2cc993a700560b
Andreas Bade <badeand@badeand.com>
2023-07-25 15:58:24 +0200

    nc

--- flipperbrain-firmware/src/main.cpp
+++ flipperbrain-firmware/src/main.cpp
@@ -2,11 +2,12 @@
 #include <Bounce2.h>
 
 Bounce2::Button btn2;
+Bounce2::Button btn3;
 
 Bounce2::Button initButton(int pin)
 {
   Bounce2::Button button = Bounce2::Button();
-  button.attach(pin, INPUT_PULLUP); // USE EXTERNAL PULL-UP
+  button.attach(pin, INPUT_PULLUP);
   button.interval(5);
   button.setPressedState(LOW);
   return button;
@@ -15,15 +16,21 @@ Bounce2::Button initButton(int pin)
 void setup()
 {
   Serial.begin(9600);
-
   btn2 = initButton(2);
+  btn3 = initButton(3);
 }
 
 void loop()
 {
   btn2.update();
+  btn3.update();
   if (btn2.pressed())
   {
-    Serial.println("Button pressed!");
+    Serial.println("Button 2 pressed!");
+  }
+
+  if (btn3.pressed())
+  {
+    Serial.println("Button 3 pressed!");
   }
 }
