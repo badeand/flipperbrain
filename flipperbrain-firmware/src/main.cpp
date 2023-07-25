d4d2a537cb0464f1987c5dbf31d087752f6f9873
Andreas Bade <badeand@badeand.com>
2023-07-25 16:22:33 +0200

    nc

--- flipperbrain-firmware/src/main.cpp
+++ flipperbrain-firmware/src/main.cpp
@@ -4,6 +4,28 @@
 Bounce2::Button btn2;
 Bounce2::Button btn3;
 
+class Output
+{
+public:
+  Output::Output(int _pin)
+  {
+    pin = _pin;
+    pinMode(pin, OUTPUT);
+  }
+
+  void turnOn();
+
+private:
+  int pin;
+};
+
+void Output::turnOn()
+{
+  digitalWrite(pin,HIGH);
+}
+
+Output* led8;
+
 Bounce2::Button initButton(int pin)
 {
   Bounce2::Button button = Bounce2::Button();
@@ -18,6 +40,8 @@ void setup()
   Serial.begin(9600);
   btn2 = initButton(2);
   btn3 = initButton(3);
+
+  led8 = &Output(8);
 }
 
 void loop()
@@ -27,6 +51,7 @@ void loop()
   if (btn2.pressed())
   {
     Serial.println("Button 2 pressed!");
+    led8->turnOn();
   }
 
   if (btn3.pressed())
